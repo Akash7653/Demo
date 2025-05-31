a220c4362e57ccc37e813bbee01244d7494274ce
Akash Koravena <akashkoravena@gmail.com>
2025-05-31 11:00:32 +0530

    Fix: Updated Signup.js to send boolean for previousLoan

--- borrower-frontend/src/Signup.js
+++ borrower-frontend/src/Signup.js
@@ -64,7 +64,7 @@ const Signup = () => {
     }
 
     try {
-      const res = await axios.post("https://demo-yt1z.onrender.com/api/auth/register", form);
+      const res = await axios.post("https://demos-nf7q.onrender.com/api/auth/register", form);
       setMessage(res.data.message || "User registered successfully");
       localStorage.setItem("token", res.data.token);
       setForm(initialState);
